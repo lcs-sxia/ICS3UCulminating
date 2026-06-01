c61640dc7211e7b3e71736b8589d4560733ce1b9
Steven Xia <sxia@lcs.on.ca>
2026-06-01 11:27:05 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by Steven Xia on 2026/6/1.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
